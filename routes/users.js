53992e22f1b73b8779483af98103769e03a4880f
Yasmeenerm <yasmeenahaa@gmail.com>
2017-06-27 12:53:39 -0400

    Changes 1 by Yasmeen

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,30 @@ const jwt = require('jsonwebtoken');
 const config = require('../config/database');
 const User = require('../models/user');
 
+
+//redirect directive
+// (function(){
+
+//     'use strict';
+//     function ngRedirectTo($window) {
+
+//         return {
+
+//             restrict: 'A',
+//             link: function(scope, element, attributes) {
+//                 element.bind('click', function (event) {
+//                     //assign ng-Redirect-To attribute value to location
+//                     $window.location.href = attributes.ngRedirectTo;
+//                 });
+//             }
+//         };
+//     }
+//     angular.module('app').directive('ngRedirectTo', ngRedirectTo);
+//     //inject $window service for redirection
+//     redirectTo.$inject = ['$window'];
+// }());
+
+
 //register
 router.post('/register', (req, res, next) => {
 	let newUser = new User({
