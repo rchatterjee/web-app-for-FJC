7cda42a9d488d6724a6c5adb19a7bbac32507ab2
Yasmeenerm <yasmeenahaa@gmail.com>
2017-06-30 09:59:11 -0400

    escapeBtn

--- angular-src/src/app/components/navbar/navbar.js
+++ angular-src/src/app/components/navbar/navbar.js
@@ -1,2 +1,21 @@
+//module
+angular.module('OCDV',[
+	])
+	//define controller
+	.controller('MainController', function($scope){
 
-// (window.location.replace(url);
+		//Escape button
+		function getAway() {
+	    	// opens new tab
+	    	window.open("http://weather.com", "_blank");
+	    	// Replaces current site with another site
+	    	window.location.replace("google.com");
+	   		$("body").html('');
+		}
+
+	   		//make function available to view
+	   		$scope.getAway = getAway;
+
+
+
+	});
