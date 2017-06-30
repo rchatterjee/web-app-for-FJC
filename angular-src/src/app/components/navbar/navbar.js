4776f9adc4f53ed4f3a12df98a339f0b613edacc
Yasmeenerm <yasmeenahaa@gmail.com>
2017-06-30 10:07:43 -0400

    escapeBtn

--- angular-src/src/app/components/navbar/navbar.js
+++ angular-src/src/app/components/navbar/navbar.js
@@ -5,16 +5,34 @@ angular.module('OCDV',[
 	.controller('MainController', function($scope){
 
 		//Escape button
-		function getAway() {
-	    	// opens new tab
-	    	window.open("http://weather.com", "_blank");
-	    	// Replaces current site with another site
-	    	window.location.replace("google.com");
-	   		$("body").html('');
-		}
 
-	   		//make function available to view
-	   		$scope.getAway = getAway;
+		(function exit(){
+        if (document.getElementById('button').clicked == true){
+
+            window.open("http://weather.com", "_blank");
+
+            window.location.replace(url);
+        }
+
+        $scope.exit= exit;
+    });
+
+
+
+
+//-------------
+//	no work
+//--------------
+		// function getAway() {
+	 //    	// opens new tab
+	 //    	window.open("http://weather.com", "_blank");
+	 //    	// Replaces current site with another site
+	 //    	window.location.replace("google.com");
+	 //   		$("body").html('');
+		// }
+
+	 //   		//make function available to view
+	 //   		$scope.getAway = getAway;
 
 
 
